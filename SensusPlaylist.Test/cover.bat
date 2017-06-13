7223014794e7e0003344d63626417a144f48b154
James Fox <James.Fox@hitachiconsulting.com>
2017-06-13 18:23:24 +0100

    adding cobertura coverage reporting

--- SensusPlaylist.Test/cover.bat
+++ SensusPlaylist.Test/cover.bat
@@ -3,15 +3,21 @@
 SET dotnet="dotnet.exe"  
 SET opencover="%USERPROFILE%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe"
 SET reportgenerator="%USERPROFILE%\.nuget\packages\ReportGenerator\2.5.2\tools\ReportGenerator.exe"
+SET opencoverConverter="%USERPROFILE%\.nuget\packages\OpenCoverToCoberturaConverter\0.2.6\tools\OpenCoverToCoberturaConverter.exe"
 
-SET targetargs="test --configuration Test"  
+SET targetargs="test SensusPlaylist.Test\SensusPlaylist.Test.csproj --configuration Test"  
 SET filter="+[SensusPlaylist]* -[*.Test]* -[xunit.*]* -[FluentValidation]*" 
-SET coveragefile=Coverage.xml
-SET coveragedir=Coverage
+SET coveragefile=SensusPlaylist.Test\Coverage.xml
+SET coberturaCoverageFile=SensusPlaylist.Test\Cobertura-Coverage.xml
+SET coveragedir=SensusPlaylist.Test\Coverage
 
 REM Run code coverage analysis  
 %opencover% -oldStyle -register:user -target:%dotnet% -output:%coveragefile% -filter:%filter% -targetargs:%targetargs% -skipautoprops
-REM Generate the report  
+
+REM Generate the Cobertura Report
+%opencoverConverter% -input:%coveragefile% -output:%coberturaCoverageFile% -sources:..
+
+REM Generate the HTML report  
 %reportgenerator% -targetdir:%coveragedir% -reporttypes:Html;Badges -reports:%coveragefile% -verbosity:Error
 
 REM Open the report  
