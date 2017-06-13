52db3021129d428f1f06243a00637c2d7335b125
James Fox <James.Fox@hitachiconsulting.com>
2017-06-13 19:59:44 +0100

    fixing up coverage report css inlining

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var inlinesource = require('gulp-inline-source');
 
 gulp.task('inlinesource', function () {
-    return gulp.src('./Sensusplaylist.Test/Coverage/*.html')
+    return gulp.src('./Sensusplaylist.Test/Coverage/*.htm')
         .pipe(inlinesource({attribute: false}))
         .pipe(gulp.dest('./Sensusplaylist.Test/Coverage-Inline'));
 });
