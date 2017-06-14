4e3b5647190aa540dc1bee365dd3525777d2b3ca
James Fox <James.Fox@hitachiconsulting.com>
2017-06-14 16:17:38 +0100

    pushing before swapping machines

--- SensusPlaylist.Test/PlaylistReaderTest.cs
+++ SensusPlaylist.Test/PlaylistReaderTest.cs
@@ -16,5 +16,14 @@ namespace SensusPlaylist.Test
                 sut.ReadAll(null);
             });
         }
+
+        private string GetTestPlaylist()
+        {
+            return "#EXTM3U" + "\r\n" +
+                    "#EXTINF:255,Simple Song - The Shins" + "\r\n" +
+                    "D:\\shlad\\Music\\iTunes\\Music\\The Shins\\Port Of Morrow\\02 Simple Song.m4a" + "\r\n" +
+                    "#EXTINF:286,Roscoe - Midlake" + "\r\n" +
+                    "D:\\shlad\\Music\\iTunes\\Music\\Midlake\\The Trials Of Van Occupanther\01 Roscoe.m4a";
+        }
     }
 }
