765a36402ccfc894b78acb3743a598b0a69caaf8
James Fox <James.Fox@hitachiconsulting.com>
2017-06-15 14:21:36 +0100

    first PlaylistWriter tests

--- SensusPlaylist.Test/PlaylistWriterTest.cs
+++ SensusPlaylist.Test/PlaylistWriterTest.cs
@@ -1,7 +1,18 @@
+using Xunit;
+
+using System;
+using System.IO;
+
 namespace SensusPlaylist.Test
 {
     public class PlaylistWriterTest
     {
-        
+        [Fact]
+        public void WriteAll_PlaylistNull_ThrowsException()
+        {
+            PlaylistWriter sut = new PlaylistWriter(new MemoryStream());
+
+            Assert.Throws<ArgumentNullException>(() => sut.WriteAll(null));
+        }
     }
 }
